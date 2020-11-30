acf5910c6553e6e4cc6569426f032201473591c9
khoaHyh <khoahuynh.toronto@gmail.com>
2020-11-30 16:57:12 -0500

    Feat: Add anchor links for home page
    Install anchor-link plugin and use for links.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-anchor-links`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/constants/links.js
+++ src/constants/links.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
 
 // Each item is an object to have navigation links all in one place
 const data = [
@@ -35,7 +35,7 @@ const tempLinks = data.map(link => {
   return (
     <li key={link.id}>
       {/* <Link to={link.url}>{link.text}</Link> */}
-      <Link to={link.url}>{link.text}</Link>
+      <AnchorLink to={link.url} title={link.text} />
     </li>
   )
 })
