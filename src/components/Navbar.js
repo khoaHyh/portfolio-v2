ed3a71814d68eff4cd19ab70f1114035cb034824
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-02 16:37:21 -0500

    Feat: Add hide navbar on scroll down
    Navbar will hide on scroll down and show on scroll up.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,50 @@
-import React from "react"
-import { FaAlignRight } from "react-icons/fa"
-import PageLinks from "../constants/links"
+import React, { Component } from "react";
+import { FaAlignRight } from "react-icons/fa";
+import PageLinks from "../constants/links";
 
-const Navbar = ({ toggleSidebar }) => {
-  return <nav className="navbar">
-    <div className="nav-center">
-      <div className="nav-header">
-        <button type="button" className="toggle-btn" 
-        onClick={toggleSidebar}>
-          <FaAlignRight></FaAlignRight>
-        </button>
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prevScrollPos: window.pageYOffset,
+      visible: true
+    }
+  }
+
+  // Mounts component when user is scrolling
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  // Unmounts component when user is not scrolling
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { prevScrollPos } = this.state;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPos > currentScrollPos;
+
+    this.setState({
+      prevScrollPos: currentScrollPos,
+      visible
+    });
+  }
+
+  render() {
+    const { toggleSidebar } = this.props;
+    return <nav className={`navbar ${this.state.visible ? "" : "navbar--hidden"}`}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <button type="button" className="toggle-btn" 
+          onClick={toggleSidebar}>
+            <FaAlignRight></FaAlignRight>
+          </button>
+        </div>
+        <PageLinks styleClass="nav-links"></PageLinks>
       </div>
-      <PageLinks styleClass="nav-links"></PageLinks>
-    </div>
-  </nav>
+    </nav>
+  }
 }
-
-export default Navbar
