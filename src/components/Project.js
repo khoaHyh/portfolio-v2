210258068a27c0c3bd6b6ac670bd601deb3d48fe
khoaHyh <khoahuynh.toronto@gmail.com>
2021-05-08 18:27:39 -0400

    Add conditional rendering for project buttons

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,34 +8,35 @@ const Project = ({ description, title, github, stack, demo, image, index }) => {
   // // console log unique props to check for errors
   // console.log({ title, demo, github, image })
 
-  return <article className="project">
-    {image && 
-
-    <Image fluid={image.childImageSharp.fluid} 
-      className="project-img" />
-    }
-    <div className="project-info">
-      <span className="project-number">0{index + 1}.</span>
-      <h3>{title || "default title"}</h3>
-      <p className="project-description">
-        {description}
-      </p>
-      <div className="project-stack">
-        {stack.map((item) => {
-          
-          return <span key={item.id}>{item.title}</span>
-        })}
+  return (
+    <article className="project">
+      {image && (
+        <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      )}
+      <div className="project-info">
+        <span className="project-number">0{index + 1}.</span>
+        <h3>{title || "default title"}</h3>
+        <p className="project-description">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+        <div className="project-links">
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <FaGithubSquare className="project-icon" />
+            </a>
+          )}
+          {demo && (
+            <a href={demo} target="_blank" rel="noopener noreferrer">
+              <FaEye className="project-icon" />
+            </a>
+          )}
+        </div>
       </div>
-      <div className="project-links">
-        <a href={github} target="_blank" rel="noopener noreferrer">
-          <FaGithubSquare className="project-icon" />
-        </a>
-        <a href={demo} target="_blank" rel="noopener noreferrer">
-          <FaEye className="project-icon" />
-        </a>
-      </div>
-    </div>
-  </article>
+    </article>
+  )
 }
 
 // typechecking
